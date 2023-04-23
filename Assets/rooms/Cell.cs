2b5d428eb4c9a8df02380f7392164639ecafdce4
MiguelHRLX <miguel.huamani.r@uni.pe>
2023-04-23 17:26:06 -0500

    vena mesh collider

--- Assets/rooms/Cell.cs
+++ Assets/rooms/Cell.cs
@@ -5,34 +5,40 @@ using UnityEngine;
 public class Cell : MonoBehaviour
 {   private Rigidbody rigidbody;
     [SerializeField] private float velocity;
-    Vector3 inicial;
-    Vector3 final;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = gameObject.GetComponent<Rigidbody>();
-        rigidbody.velocity = (gameObject.transform.right+gameObject.transform.up* Random.Range(0, 0.1f) + gameObject.transform.forward * Random.Range(0, 0.1f)).normalized * velocity;
+        rigidbody.velocity = (gameObject.transform.right+VarianceVelocity(gameObject)).normalized * velocity;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(rigidbody.velocity.magnitude);
-        Debug.Log(rigidbody.velocity);
+        //Debug.Log(rigidbody.velocity.magnitude);
+        //Debug.Log(rigidbody.velocity);
+        if (rigidbody.velocity.magnitude != velocity) rigidbody.velocity = rigidbody.velocity.normalized * velocity;
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.CompareTag("impulce"))
         {
             Debug.Log("impulce");
-            rigidbody.velocity = other.transform.right * velocity;
+            rigidbody.velocity = ((other.transform.right)+VarianceVelocity(other.gameObject)).normalized* velocity;
         }
     }
     private void OnCollisionEnter(Collision collision)
     {
         Vector3 initialDirection = gameObject.transform.right;
-        rigidbody.velocity = rigidbody.velocity.normalized * velocity;
-       // gameObject.transform.right = rigidbody.velocity.normalized;
+        rigidbody.velocity = (rigidbody.velocity+VarianceVelocity(gameObject)).normalized* velocity;
+        // gameObject.transform.right = rigidbody.velocity.normalized;
+        // if (collision != null) Debug.Log("colisión");
+        //if (collision.transform.CompareTag("Finish")) Debug.Log("Vena");
+
+    }
+    private Vector3 VarianceVelocity(GameObject reference)
+    { 
+        return reference.transform.up * Random.Range(-0.3f, 0.3f) + reference.transform.forward * Random.Range(-0.3f, 0.3f);
     }
 }
